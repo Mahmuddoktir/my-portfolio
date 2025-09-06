5fe60c985fdba41166a86b2aa0187fc8033cfbd0
Mahmud <mahmuddoktir@gmail.com>
2025-09-06 14:49:32 +0500

    g tugirlandi

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function HomePage() {
       {/* Matn ustiga chiqishi uchun */}
       <div className="absolute inset-0 bg-black/50 flex flex-col items-center justify-center text-center text-white">
         <h1 className="text-5xl font-bold">Rino Jarroh Dr Samandar</h1>
-        <p className="mt-4 text-lg">Tomoq | Og'iz & Burun</p>
+        <p className="mt-4 text-lg">Tomoq | Og`iz & Burun</p>
         <div className="bg-cyan-700 mt-50 p-5 rounded-2xl hover:bg-cyan-500">
           <button className="text-2xl text-blue cursor-pointer">
             Doctor Qabuliga yozilish
