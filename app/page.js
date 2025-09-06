125bce6d8e1fa473fd7df66a82ef28347bd87a44
Mahmud <mahmuddoktir@gmail.com>
2025-09-06 14:38:58 +0500

    rasm tugirlandi

--- app/page.js
+++ app/page.js
@@ -9,13 +9,18 @@ export default function HomePage() {
         alt="Hero background"
         fill
         priority
-        className="object-cover"
+        // className="object-cover"
       />
 
       {/* Matn ustiga chiqishi uchun */}
       <div className="absolute inset-0 bg-black/50 flex flex-col items-center justify-center text-center text-white">
-        <h1 className="text-5xl font-bold">Mening Portfolio Saytim</h1>
-        <p className="mt-4 text-lg">Frontend Developer | React & Next.js</p>
+        <h1 className="text-5xl font-bold">Rino Jarroh Dr Samandar</h1>
+        <p className="mt-4 text-lg">Tomoq | Og'iz & Burun</p>
+        <div className="bg-cyan-700 mt-50 p-5 rounded-2xl hover:bg-cyan-500">
+          <button className="text-2xl text-blue cursor-pointer">
+            Doctor Qabuliga yozilish
+          </button>
+        </div>
       </div>
     </section>
   );
