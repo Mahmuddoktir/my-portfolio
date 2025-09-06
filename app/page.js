3b41df7b7405d51109261f1296f6d92627529e9d
Mahmud <mahmuddoktir@gmail.com>
2025-09-06 11:18:42 +0500

    rasm joylandi

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="p-10 text-center">
+      <Image
+        src="/profile.jpg" // public papkadan oladi
+        alt="Mening rasmiym"
+        width={150}
+        height={150}
+        className="mx-auto rounded-full border-4 border-gray-300"
+      />
       <h1 className="text-4xl font-bold">Salom, men Mahmud!</h1>
       <p className="mt-4 text-lg text-gray-600">
         Bu mening portfolio saytim. Men frontend developer’mан 🚀
